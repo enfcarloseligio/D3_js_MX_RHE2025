e9be4443c58911aef8542bb598cf8e48d737cd56
Juan Carlos De La Cruz Eligio <enfcarloseligio@gmail.com>
2025-06-18 15:30:40 -0600

    sticky

--- scripts/inyectar-layout.js
+++ scripts/inyectar-layout.js
@@ -4,6 +4,16 @@ document.addEventListener("DOMContentLoaded", () => {
     .then(res => res.text())
     .then(html => {
       document.body.insertAdjacentHTML("afterbegin", html);
+
+      // Inyectar banner justo antes del <main>
+      fetch("../componentes/banner.html")
+        .then(res => res.text())
+        .then(banner => {
+          const main = document.querySelector("main");
+          if (main) {
+            main.insertAdjacentHTML("beforebegin", banner);
+          }
+        });
     });
 
   // Inyectar footer
