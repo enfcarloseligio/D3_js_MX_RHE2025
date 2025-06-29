a43b282448179c270f85b6594ea0e5d8d1b8c465
enfcarloseligio <enfcarloseligio@gmail.com>
2025-06-29 05:29:10 -0600

    metadatos

--- scripts/inyectar-layout.js
+++ scripts/inyectar-layout.js
@@ -1,25 +1,21 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // Inyectar header
-  fetch("../componentes/header.html")
-    .then(res => res.text())
-    .then(html => {
-      document.body.insertAdjacentHTML("afterbegin", html);
+(async function inyectarLayout() {
+  try {
+    // Inyectar header
+    const headerHtml = await fetch("../componentes/header.html").then(res => res.text());
+    document.body.insertAdjacentHTML("afterbegin", headerHtml);
 
-      // Inyectar banner justo antes del <main>
-      fetch("../componentes/banner.html")
-        .then(res => res.text())
-        .then(banner => {
-          const main = document.querySelector("main");
-          if (main) {
-            main.insertAdjacentHTML("beforebegin", banner);
-          }
-        });
-    });
+    // Inyectar banner justo antes del <main>
+    const bannerHtml = await fetch("../componentes/banner.html").then(res => res.text());
+    const main = document.querySelector("main");
+    if (main) {
+      main.insertAdjacentHTML("beforebegin", bannerHtml);
+    }
 
-  // Inyectar footer
-  fetch("../componentes/footer.html")
-    .then(res => res.text())
-    .then(html => {
-      document.body.insertAdjacentHTML("beforeend", html);
-    });
-});
+    // Inyectar footer
+    const footerHtml = await fetch("../componentes/footer.html").then(res => res.text());
+    document.body.insertAdjacentHTML("beforeend", footerHtml);
+
+  } catch (error) {
+    console.error("Error al inyectar layout:", error);
+  }
+})();
